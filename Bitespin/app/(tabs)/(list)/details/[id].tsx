dc80441654a8f3266ca97e5d110a9c8456a2ed3c
DeftSlyNinja <christopher_masson@hotmail.com>
2025-06-02 12:27:26 -0700

    formatted details page to use dummy data

--- Bitespin/app/(tabs)/(list)/details/[id].tsx
+++ Bitespin/app/(tabs)/(list)/details/[id].tsx
@@ -1,17 +1,43 @@
-import { View, Text, Button } from 'react-native';
 import { router, useLocalSearchParams } from 'expo-router';
+import restaurantData from '../../../../data/restaurantsList.json'
+import { Heading } from '@/components/ui/heading';
+import { Box } from '@/components/ui/box';
+import { Text } from '@/components/ui/text';
+import { Image } from '@/components/ui/image';
+import { Fab, FabIcon } from '@/components/ui/fab';
+import { ArrowLeftIcon } from '@/components/ui/icon';
 
 
 export default function DetailsScreen() {
   const { id } = useLocalSearchParams();
+  const restaurant = restaurantData.find((item) => item.id === id)
 
   return (
-    <View>
-      <Text>Restaurant Details for #{id}</Text>
-      <Text>[Image Placeholder]</Text>
-      <Text>[Info: Genre, Rating, Location, Visited]</Text>
-      <Text>[Description Placeholder]</Text>
-      <Button title='Return' onPress={() => router.back()}/>
-    </View>
+
+    <Box className='flex-1 p-4'>
+      <Heading size="3xl" className="self-center">{restaurant?.title}</Heading>
+      <Box className='flex-2 m-2 items-center justify-items-center'>
+        <Image
+        className='rounded-3xl'
+          size='2xl'
+          source={{
+            uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          }}
+          alt="image"
+        />
+      </Box>
+      <Box className='flex-1'>
+        <Text>Genre: {restaurant?.genre}</Text>
+        <Text>Location: {restaurant?.location}</Text>
+        <Text>Rating: {restaurant?.rating}</Text>
+        <Text>Visited: {restaurant?.visited ? "Visited" : "Not yet visited"}</Text>
+      </Box>
+      <Fab
+        size='lg'
+        className='origin-bottom-right dark:bg-zinc-700'
+        onPress={() => router.back()}>
+        <FabIcon as={ArrowLeftIcon} color="white"></FabIcon>
+      </Fab>
+    </Box>
   );
 }
--- Bitespin/app/(tabs)/(list)/list.tsx
+++ Bitespin/app/(tabs)/(list)/list.tsx
@@ -1,4 +1,4 @@
-import { TextInput } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 import { FlatList } from 'react-native';
 import restaurantsData from '../../../data/restaurantsList.json';
@@ -7,7 +7,9 @@ import { Heading } from '@/components/ui/heading';
 import { Box } from '@/components/ui/box';
 import { Text } from '@/components/ui/text';
 import { Fab, FabIcon } from '@/components/ui/fab';
-import { EditIcon } from '@/components/ui/icon';
+import { AddIcon, EditIcon, FavouriteIcon } from '@/components/ui/icon';
+import { Button, ButtonIcon } from '@/components/ui/button';
+import { HStack } from '@/components/ui/hstack';
 
 export default function ListScreen() {
   const router = useRouter();
@@ -22,23 +24,39 @@ export default function ListScreen() {
         data={restaurantsData}
         keyExtractor={(item) => item.id} 
         renderItem={({ item }) => (
-          <Card size="md" variant="elevated" className="m-3">
-            <Heading size="md" className="mb-1">
-              {item.title}
-            </Heading>
-            <Text size="sm">{item.location}</Text>
-          </Card>
+          <TouchableOpacity onPress={() => router.push(`/(tabs)/(list)/details/${item.id}`)}>
+            <Card size="md" variant="elevated" className="m-3">
+              <HStack space="md">
+                <Box>
+                  <Button onPress={() => router.push(`/(tabs)/(list)/edit/${item.id}`)}>
+                    <ButtonIcon as={EditIcon}></ButtonIcon>
+                  </Button>
+                </Box>
+                <Box>
+                  <Heading size="md" className="mb-1">
+                    {item.title}
+                  </Heading>
+                  <Text size="sm">{item.location}</Text>
+                </Box>
+                <Box>
+                  <Button onPress={() =>{item.favorite}}>
+                    <ButtonIcon 
+                      as={FavouriteIcon}
+                      color={item.favorite ? "red" : "white"}
+                    />
+                  </Button>
+                </Box>
+              </HStack>
+            </Card>
+          </TouchableOpacity>
         )}>
       </FlatList>
       <Fab
         size='lg'
         className='bottom-32 dark:bg-zinc-700'
         onPress={() => router.push('/add')}>
-        <FabIcon as={EditIcon} color="white"></FabIcon>
+        <FabIcon as={AddIcon} color="white"></FabIcon>
       </Fab>
-      {/* <Button title="Edit #1" onPress={() => router.push('./edit/1')} />
-      <Button title="Delete #1" onPress={() => router.push('./delete/1')} />
-      <Button title="Add New" onPress={() => router.push('/add')} /> */}
     </Box>
   );
 }
