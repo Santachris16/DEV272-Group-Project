8d64779a71210bbf3741d46159db7634d3151a88
DeftSlyNinja <christopher_masson@hotmail.com>
2025-06-01 17:35:43 -0700

    added dummy data

--- Bitespin/app/(tabs)/(list)/list.tsx
+++ Bitespin/app/(tabs)/(list)/list.tsx
@@ -1,5 +1,7 @@
 import { View, Text, TextInput, Button } from 'react-native';
 import { useRouter } from 'expo-router';
+import { FlatList } from 'react-native';
+import restaurantsData from 'Bitespin/tempfiles/restaurantsList.json';
 
 export default function ListScreen() {
   const router = useRouter();
@@ -9,6 +11,12 @@ export default function ListScreen() {
       <Text>Restaurant List</Text>
       <TextInput placeholder="Search Restaurants" />
       <Text>• Restaurant #1</Text>
+      <FlatList
+        data={}
+        keyExtractor={}
+        renderItem={}>
+        
+      </FlatList>
       <Button title="Edit #1" onPress={() => router.push('./edit/1')} />
       <Button title="Delete #1" onPress={() => router.push('./delete/1')} />
       <Button title="Add New" onPress={() => router.push('/add')} />
